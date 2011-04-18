6b4e1a16eea72c4c37b6eda8fbef8b7ce3247ac2
Andrea D'Ippolito <adedip@gmail.com>
2011-04-18 11:49:17 +0200

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fourfour::Application.config.secret_token = 'cc8bd21ca59bf626c4ba9c8b8d5cdad2dde8b2c0287283a965d532a3332063ecf237d8e79c5671fc84946dc770dc2766572c995eb5e2d9a4bbd30fb66cade940'
