f1c3c3839b1f8153bcb0fa5db3c5141219d16f41
Andrea D'Ippolito <adedip@gmail.com>
2011-04-19 16:47:49 +0200

    js player event

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,4 @@ module ApplicationHelper
     uri = URI.parse("http://localhost:9292/faye")
     Net::HTTP.post_form(uri,:message => move.to_json )
   end
-  
-  def new_player(channel, &block)
-    second_player = {:channel => channel, :data => capture(&block)}
-    uri = URI.parse("http://localhost:9292/faye")
-    Net::HTTP.post_form(uri,:message => second_player.to_json )
-  end
 end
