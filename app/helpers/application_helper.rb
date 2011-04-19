13f659790d50cfaf32baf0d6d68899f42f8a18e1
Andrea D'Ippolito <adedip@gmail.com>
2011-04-19 12:18:14 +0200

    faye config

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def broadcast(channel, &block)
+    move = {:channel => channel, :data => capture(&block)}
+    uri = URI.parse("http://localhost:9292/faye")
+    Net::HTTP.post_form(uri,:message => move.to_json )
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,6 @@
 $(function(){
-var faye = new Faye.Client('http://localhost:9292/faye');
-
+    var faye = new Faye.Client('http://localhost:9292/faye');
+    faye.subscribe("/board", function(data){
+        alert(data);
+    });
 });
