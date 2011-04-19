79656d2180a6d4e4391d6edfeeda90b6166f2ea4
Andrea D'Ippolito <adedip@gmail.com>
2011-04-19 15:46:48 +0200

    js player event

--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -1,2 +1,7 @@
 module GameHelper
+  def new_player(channel, player)
+    second_player = {:channel => channel, :data => player}
+    uri = URI.parse("http://localhost:9292/faye")
+    Net::HTTP.post_form(uri,:message => second_player.to_json )
+  end
 end
