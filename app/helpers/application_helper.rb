ccc117b246fbbbc9b426963f3008c4d437f07e65
Andrea D'Ippolito <adedip@gmail.com>
2011-04-20 10:27:36 +0200

    js player event

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def broadcast(channel, &block)
     move = {:channel => channel, :data => capture(&block)}
-    uri = URI.parse("http://localhost:9292/faye")
+    uri = URI.parse("http://192.168.1.185:9292/faye")
     Net::HTTP.post_form(uri,:message => move.to_json )
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(function(){
-    var faye = new Faye.Client('http://localhost:9292/faye');
+    var faye = new Faye.Client('http://192.168.1.185:9292/faye');
     faye.subscribe("/board", function(data){
         eval(data);
     });
