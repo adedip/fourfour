270dccee7f2e8e8c36ef29a3bc916506864e9004
Andrea D'Ippolito <adedip@gmail.com>
2011-04-21 16:27:05 +0200

    user two checks

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -13,7 +13,7 @@ class GameController < ApplicationController
   def move
     board = Board.find params[:id]
     if board.user_two.nil?
-      flash[:notice] = "aspetta il secondo giocatore!"
+    render(:update) { |page| page.alert("wait for user two") }
     else
       if board.move current_user, params[:x], params[:y]
         board.save
@@ -28,7 +28,7 @@ class GameController < ApplicationController
   private
   def broadcast(channel, block)
     move = {:channel => channel, :data => block}
-    uri = URI.parse("http://localhost:9292/faye")
+    uri = URI.parse("http://192.168.1.75:9292/faye")
     Net::HTTP.post_form(uri, :message => move.to_json)
   end
 end
